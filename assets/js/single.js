febf6428ef3227687d6fc0bb5711f31f2701c5f1
dhalladay <dave.halladay@outlook.com>
2021-11-27 18:37:25 -0700

    add warning if more than 30 repos

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +10,10 @@ var getRepoIssues = function(repo) {
       response.json().then(function(data) {
         //pass response datat to dom function
         displayIssues(data);
+        //check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     }
     else {
@@ -17,7 +22,7 @@ var getRepoIssues = function(repo) {
   });
 };
 
-getRepoIssues("dhalladay/daily-calendar");
+getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
   if (issues.length === 0) {
@@ -55,3 +60,16 @@ var displayIssues = function(issues) {
     issueContainerEl.appendChild(issueEl);
   };
 };
+
+var displayWarning = function(repo) {
+  //add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on Github.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  //append to warning container
+  limitWarningEl.appendChild(linkEl);
+}
