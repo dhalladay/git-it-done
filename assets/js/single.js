04a4c5ad20b8d2d4a452fc41cfb1ee33f747efdc
dhalladay <dave.halladay@outlook.com>
2021-11-27 19:39:28 -0700

    added dynamic functionality to homepage and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,10 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-
+  //make a get request to url
   fetch(apiUrl).then(function(response) {
     //request was successful
     if (response.ok) {
@@ -17,12 +18,11 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      alert("There was a problem with your request!")
+      //if not successful, redirect to the homepage
+      document.location.replace("./index.html");
     }
   });
 };
-
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
   if (issues.length === 0) {
@@ -72,4 +72,21 @@ var displayWarning = function(repo) {
 
   //append to warning container
   limitWarningEl.appendChild(linkEl);
-}
+};
+
+var getRepoName = function() {
+  //grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if(repoName) {
+    //display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    //if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
+
+getRepoName();
