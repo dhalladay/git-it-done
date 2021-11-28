eb7d99774297145cc73684ab6f01a1b2105b9ecb
dhalladay <dave.halladay@outlook.com>
2021-11-27 20:20:14 -0700

    added lanugage repo search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
   //format the gihub api url
@@ -86,3 +87,30 @@ var displayRepos = function (repos, searchTerm) {
   console.log(repos);
   console.log(searchTerm);
 };
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues"
+  fetch(apiUrl)
+  .then(function(response) {
+    //request was successful
+    if(response.ok) {
+      response.json().then(function(data) {
+      displayRepos(data.items, language);
+      });
+    } 
+    else {
+      alert("Error: GitHub user not found");
+    }
+  });
+};
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  if(language) {
+    getFeaturedRepos(language);
+    //clear old content
+    repoContainerEl.textContent = "";
+  }
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
